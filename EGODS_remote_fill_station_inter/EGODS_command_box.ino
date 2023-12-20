a80d8ffc7278cce4e2620b68bcfe0f754a591aec
CI0817 <80679386+CI0817@users.noreply.github.com>
2023-12-20 20:54:49 +0700

    Add files via upload

---
+++ EGODS_remote_fill_station_inter/EGODS_command_box.ino
@@ -0,0 +1,163 @@
+/*
+filename      : EGODS_command_box.ino
+Written By    : Chhay Irseng
+Created Date  : 13/12/2023
+Description   : Let a microncontroller (Arduino Uno R4 Minima) constantly receives 
+                pressure data readings wireless (using LoRA technology) from another microcontroller 
+                and sends control signals to the other micrcontroller to actuate three separate valves.
+*/
+
+// Import libraries
+#include <SPI.h>
+#include <LoRa.h>
+
+// Define microcontroller pins
+  /* Switches */
+const int FILL_SW = 4;
+const int DUMP_SW = 7;
+const int CHECK_SW = 8;
+
+
+  /* LoRA comms */
+const int LORA_G0 = 3;
+const int LORA_SCK = 13;
+const int LORA_MISO = 12;
+const int LORA_MOSI = 11;
+const int LORA_CS = 10;
+const int LORA_RST = 2;
+
+
+// Declare and initialise constants
+const long LORA_FREQ = 915E6;     // LoRA's transceiver module operaating frequency
+
+// Declare and initialise global variables
+  /* Local and destination addresses */
+byte local_address = 0xCC;
+byte destination_address = 0xBB;
+
+  /* code to be sent */
+byte code = 0;
+String eight_bit_code = "";
+/* code encode/decode:
+  (0) 0b00000000  : fill valve OFF, dump valve OFF, check valve OFF (off state)
+  (1) 0b00000001  : fill valve OFF, dump valve OFF, check valve ON  (don't care state)
+  (2) 0b00000010  : fill valve OFF, dump valve ON,  check valve OFF (dumping state)
+  (3) 0b00000011  : fill valve OFF, dump valve ON,  check valve ON  (forbidden state - bad practice?)
+  (4) 0b00000100  : fill valve ON,  dump valve OFF, check valve OFF (fill-ready state)
+  (5) 0b00000101  : fill valve ON,  dump valve OFF, check valve ON  (filling state)
+  (6) 0b00000110  : fill valve ON,  dump valve ON,  check valve OFF (forbidden state - draining the supply tank)
+  (7) 0b00000111  : fill valve ON,  dump valve ON,  check valve ON  (forbidden state - draining the supply tank + bad practice)
+*/
+
+  /* Data to be received */
+String data = "";
+
+  /* Set up and loop function success checker */
+byte set_up_success = 0;
+byte loop_fail_once = 0;
+
+  /* Time intervals and last sent */
+long last_send_time = 0;        // last send time
+int interval = 1000;          // interval between sends
+
+void setup() {
+  // Set up Serial baud rate
+  Serial.begin(9600); while (!Serial); // wait for Serial to set up properly
+
+  // Set up switch pins
+  pinMode(FILL_SW, INPUT);
+  pinMode(DUMP_SW, INPUT);
+  pinMode(CHECK_SW, INPUT);
+
+  // Set up LoRA module pins
+  LoRa.setPins(LORA_CS, LORA_RST, LORA_G0);
+
+  // Set up LoRA operating frequency
+  if (!LoRa.begin(LORA_FREQ)) {
+    Serial.println("SET_UP_ERROR  : LORA module failed to set up operating frequency.");
+    return;
+  }
+
+  // Set up call-back function for receiving packets
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+
+  // Finish setting up everything
+  set_up_success = 1;
+}
+
+void loop() {
+  // Verify that the set up function ran properly and the loop has not failed
+  if (loop_fail_once) {
+    return;
+  }
+  if (!set_up_success) {
+    Serial.println("LOOP_ERROR    : the set_up function failed to complete properly, loop function cannot proceed.");
+    loop_fail_once = 1;
+    return;
+  }
+  // If everything ran properly, proceed to run the loop...
+  if (millis() - last_send_time > interval) {
+      // Get the code by reading the switches
+    code_generator();
+    eight_bit_code_format(code);
+    Serial.println("code: " + eight_bit_code);
+      // Send the code to the actuator box
+    sendMessage(code);
+      // Update the last send time and interval
+      last_send_time = millis();
+      interval = random(1000) + 500;
+      // Put the module back in receiving mode
+      LoRa.receive();
+      Serial.println("data: " + data);
+  }
+}
+
+void code_generator() {
+  byte fill_sw_state = digitalRead(FILL_SW); fill_sw_state = fill_sw_state << 2;
+  byte dump_sw_state = digitalRead(DUMP_SW); dump_sw_state = dump_sw_state << 1;
+  byte check_sw_state = digitalRead(CHECK_SW);
+  code = fill_sw_state + dump_sw_state + check_sw_state;
+}
+
+void sendMessage (byte code) {
+  LoRa.beginPacket();
+  LoRa.write(destination_address);
+  LoRa.write(local_address);
+  LoRa.write(code);
+  LoRa.endPacket();
+}
+
+void onReceive(int packet_size) {
+  if (packet_size) {
+    byte recipient_address = LoRa.read();
+    byte sender_address = LoRa.read();
+    if ((recipient_address != local_address) || (sender_address != destination_address)) {
+      Serial.println("ON RECEIVE ERROR  : packet was not meant to be received.");
+      return;
+    }
+    data = "";
+    while (LoRa.available()) {
+      data += (char)LoRa.read();
+    }
+  }
+}
+
+void eight_bit_code_format(byte code) {
+  eight_bit_code = "";
+  for (int i=7; i >-1; i--) {
+    eight_bit_code += bitRead(code,i);
+  }
+}
+
+/* ------------------------------------------------------------------------------------ */
+// // empty sketch
+// void setup() {
+//   // put your setup code here, to run once:
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+// }
